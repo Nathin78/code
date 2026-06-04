021102d21918582d7ef010eb2cde0a4674f23948
Nathin78 <nathinan30115@gmail.com>
2026-06-04 13:30:17 +0530

    feat(60-days): Add Day 02 - Reverse Singly Linked List [2026-06-04]

---
+++ contributions_60_days/day02_reverse_linked_list.py
@@ -0,0 +1,18 @@
+﻿class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_list(head):
+    prev, curr = None, head
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev
+
+if __name__ == "__main__":
+    head = ListNode(1, ListNode(2, ListNode(3)))
+    rev = reverse_list(head)
+    print(rev.val, rev.next.val, rev.next.next.val)
