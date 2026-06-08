20ed01ccf3826de844ebb5583471e1b1e3ed13e7
Nathin78 <nathinan30115@gmail.com>
2026-06-08 13:30:17 +0530

    feat(60-days): Add Day 06 - Maximum Subarray (Kadane's Algorithm) [2026-06-08]

---
+++ contributions_60_days/day06_maximum_subarray.py
@@ -0,0 +1,10 @@
+﻿def max_sub_array(nums):
+    max_current = max_global = nums[0]
+    for i in range(1, len(nums)):
+        max_current = max(nums[i], max_current + nums[i])
+        if max_current > max_global:
+            max_global = max_current
+    return max_global
+
+if __name__ == "__main__":
+    print(max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
