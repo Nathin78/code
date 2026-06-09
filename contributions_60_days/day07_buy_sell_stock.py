3f2c77cb209d0ed297a4df8921044903defc4f5f
Nathin78 <nathinan30115@gmail.com>
2026-06-09 13:30:17 +0530

    feat(60-days): Add Day 07 - Best Time to Buy and Sell Stock [2026-06-09]

---
+++ contributions_60_days/day07_buy_sell_stock.py
@@ -0,0 +1,12 @@
+﻿def max_profit(prices):
+    min_price = float('inf')
+    max_p = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_p:
+            max_p = price - min_price
+    return max_p
+
+if __name__ == "__main__":
+    print(max_profit([7, 1, 5, 3, 6, 4]))
