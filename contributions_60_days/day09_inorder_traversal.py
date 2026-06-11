7627ba950e97ebef8a6f332e8d0cf387357e39a7
Nathin78 <nathinan30115@gmail.com>
2026-06-11 13:30:17 +0530

    feat(60-days): Add Day 09 - Binary Tree Inorder Traversal [2026-06-11]

---
+++ contributions_60_days/day09_inorder_traversal.py
@@ -0,0 +1,19 @@
+﻿class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorder_traversal(root):
+    res = []
+    def helper(node):
+        if not node: return
+        helper(node.left)
+        res.append(node.val)
+        helper(node.right)
+    helper(root)
+    return res
+
+if __name__ == "__main__":
+    root = TreeNode(1, None, TreeNode(2, TreeNode(3)))
+    print(inorder_traversal(root))
