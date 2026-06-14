35d5c0eb38d693da4063f469914f2d4c3b3deb07
Nathin78 <nathinan30115@gmail.com>
2026-06-14 13:30:17 +0530

    feat(60-days): Add Day 12 - Linked List Cycle Detection [2026-06-14]

---
+++ contributions_60_days/day12_linked_list_cycle.py
@@ -0,0 +1,17 @@
+﻿class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast: return True
+    return False
+
+if __name__ == "__main__":
+    n1, n2, n3 = ListNode(3), ListNode(2), ListNode(0)
+    n1.next, n2.next, n3.next = n2, n3, n2
+    print(has_cycle(n1))
