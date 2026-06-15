cfa9a640ce0f65426b4d9c35113c83b86a5f2633
Nathin78 <nathinan30115@gmail.com>
2026-06-15 13:30:17 +0530

    feat(60-days): Add Day 13 - Climbing Stairs DP [2026-06-15]

---
+++ contributions_60_days/day13_climbing_stairs.py
@@ -0,0 +1,9 @@
+﻿def climb_stairs(n):
+    if n <= 2: return n
+    first, second = 1, 2
+    for _ in range(3, n + 1):
+        first, second = second, first + second
+    return second
+
+if __name__ == "__main__":
+    print(climb_stairs(5))
