1661780a7a0ae00780f4c367d1334ec0be9ec006
Nathin78 <nathinan30115@gmail.com>
2026-06-16 13:30:17 +0530

    feat(60-days): Add Day 14 - Lowest Common Ancestor of BST [2026-06-16]

---
+++ contributions_60_days/day14_lca_bst.py
@@ -0,0 +1,21 @@
+﻿class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def lowest_common_ancestor(root, p, q):
+    curr = root
+    while curr:
+        if p.val < curr.val and q.val < curr.val:
+            curr = curr.left
+        elif p.val > curr.val and q.val > curr.val:
+            curr = curr.right
+        else:
+            return curr
+    return None
+
+if __name__ == "__main__":
+    p, q = TreeNode(2), TreeNode(8)
+    root = TreeNode(6, p, q)
+    print(lowest_common_ancestor(root, p, q).val)
