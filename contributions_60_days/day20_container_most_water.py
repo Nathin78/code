dc37f8bd37238eed37102f0fe5948fcd2919a3bb
Nathin78 <nathinan30115@gmail.com>
2026-06-22 13:30:17 +0530

    feat(60-days): Add Day 20 - Container With Most Water [2026-06-22]

---
+++ contributions_60_days/day20_container_most_water.py
@@ -0,0 +1,12 @@
+﻿def max_area(height):
+    l, r = 0, len(height) - 1
+    max_water = 0
+    while l < r:
+        water = min(height[l], height[r]) * (r - l)
+        max_water = max(max_water, water)
+        if height[l] < height[r]: l += 1
+        else: r -= 1
+    return max_water
+
+if __name__ == "__main__":
+    print(max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]))
