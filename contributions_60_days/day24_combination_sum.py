97f825648ea0c64e8ad6ff78c0dab4946208c33a
Nathin78 <nathinan30115@gmail.com>
2026-06-26 13:30:17 +0530

    feat(60-days): Add Day 24 - Combination Sum Backtracking [2026-06-26]

---
+++ contributions_60_days/day24_combination_sum.py
@@ -0,0 +1,16 @@
+﻿def combination_sum(candidates, target):
+    res = []
+    def backtrack(remain, combo, start):
+        if remain == 0:
+            res.append(list(combo))
+            return
+        for i in range(start, len(candidates)):
+            if candidates[i] > remain: continue
+            combo.append(candidates[i])
+            backtrack(remain - candidates[i], combo, i)
+            combo.pop()
+    backtrack(target, [], 0)
+    return res
+
+if __name__ == "__main__":
+    print(combination_sum([2, 3, 6, 7], 7))
