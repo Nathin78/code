b6034f3de97e8890ac35303078ee207588e0886a
Nathin78 <nathinan30115@gmail.com>
2026-06-27 13:30:17 +0530

    feat(60-days): Add Day 25 - Permutations Backtracking [2026-06-27]

---
+++ contributions_60_days/day25_permutations.py
@@ -0,0 +1,15 @@
+﻿def permute(nums):
+    res = []
+    def backtrack(first=0):
+        if first == len(nums):
+            res.append(nums[:])
+            return
+        for i in range(first, len(nums)):
+            nums[first], nums[i] = nums[i], nums[first]
+            backtrack(first + 1)
+            nums[first], nums[i] = nums[i], nums[first]
+    backtrack()
+    return res
+
+if __name__ == "__main__":
+    print(permute([1, 2, 3]))
