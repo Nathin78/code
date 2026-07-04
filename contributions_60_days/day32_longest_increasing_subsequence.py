23056b693e5851909885b1e5ac234f27a0a268c7
Nathin78 <nathinan30115@gmail.com>
2026-07-04 13:30:17 +0530

    feat(60-days): Add Day 32 - Longest Increasing Subsequence [2026-07-04]

---
+++ contributions_60_days/day32_longest_increasing_subsequence.py
@@ -0,0 +1,12 @@
+﻿import bisect
+
+def length_of_lis(nums):
+    sub = []
+    for x in nums:
+        i = bisect.bisect_left(sub, x)
+        if i == len(sub): sub.append(x)
+        else: sub[i] = x
+    return len(sub)
+
+if __name__ == "__main__":
+    print(length_of_lis([10, 9, 2, 5, 3, 7, 101, 18]))
