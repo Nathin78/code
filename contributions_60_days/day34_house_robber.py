6dc7d2f12fd6995bd80fefdd41f1a3d0794d360d
Nathin78 <nathinan30115@gmail.com>
2026-07-06 13:30:17 +0530

    feat(60-days): Add Day 34 - House Robber Dynamic Programming [2026-07-06]

---
+++ contributions_60_days/day34_house_robber.py
@@ -0,0 +1,10 @@
+﻿def rob(nums):
+    prev1 = prev2 = 0
+    for n in nums:
+        temp = max(prev1, prev2 + n)
+        prev2 = prev1
+        prev1 = temp
+    return prev1
+
+if __name__ == "__main__":
+    print(rob([2, 7, 9, 3, 1]))
