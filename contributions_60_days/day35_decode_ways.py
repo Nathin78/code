26d18c0532fb91a959bf00afb56744d2da2ab19a
Nathin78 <nathinan30115@gmail.com>
2026-07-07 13:30:17 +0530

    feat(60-days): Add Day 35 - Decode Ways Dynamic Programming [2026-07-07]

---
+++ contributions_60_days/day35_decode_ways.py
@@ -0,0 +1,12 @@
+﻿def num_decodings(s):
+    if not s or s[0] == '0': return 0
+    dp = [0] * (len(s) + 1)
+    dp[0] = dp[1] = 1
+    for i in range(2, len(s) + 1):
+        if s[i-1] != '0': dp[i] += dp[i-1]
+        two_digit = int(s[i-2:i])
+        if 10 <= two_digit <= 26: dp[i] += dp[i-2]
+    return dp[len(s)]
+
+if __name__ == "__main__":
+    print(num_decodings("226"))
