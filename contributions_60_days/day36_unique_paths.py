0b26747e438cebd7ee4f3416cea5b6c8c4cdad33
Nathin78 <nathinan30115@gmail.com>
2026-07-08 13:30:17 +0530

    feat(60-days): Add Day 36 - Unique Paths Dynamic Programming [2026-07-08]

---
+++ contributions_60_days/day36_unique_paths.py
@@ -0,0 +1,8 @@
+﻿def unique_paths(m, n):
+    dp = [1] * n
+    for _ in range(m - 1):
+        for j in range(1, n): dp[j] += dp[j - 1]
+    return dp[-1]
+
+if __name__ == "__main__":
+    print(unique_paths(3, 7))
