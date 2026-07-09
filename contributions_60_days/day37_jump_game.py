fce1f177799ec75c66dea1bf5db9ff06e6026104
Nathin78 <nathinan30115@gmail.com>
2026-07-09 13:30:17 +0530

    feat(60-days): Add Day 37 - Jump Game Greedy Algorithm [2026-07-09]

---
+++ contributions_60_days/day37_jump_game.py
@@ -0,0 +1,9 @@
+﻿def can_jump(nums):
+    max_reach = 0
+    for i, n in enumerate(nums):
+        if i > max_reach: return False
+        max_reach = max(max_reach, i + n)
+    return True
+
+if __name__ == "__main__":
+    print(can_jump([2, 3, 1, 1, 4]))
