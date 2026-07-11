a32b93634657a93dfea4ebfd3423eba1e83cd337
Nathin78 <nathinan30115@gmail.com>
2026-07-11 13:30:17 +0530

    feat(60-days): Add Day 39 - Insert Interval [2026-07-11]

---
+++ contributions_60_days/day39_insert_interval.py
@@ -0,0 +1,14 @@
+﻿def insert_interval(intervals, newInterval):
+    res = []
+    for i, interval in enumerate(intervals):
+        if newInterval[1] < interval[0]:
+            return res + [newInterval] + intervals[i:]
+        elif newInterval[0] > interval[1]:
+            res.append(interval)
+        else:
+            newInterval = [min(newInterval[0], interval[0]), max(newInterval[1], interval[1])]
+    res.append(newInterval)
+    return res
+
+if __name__ == "__main__":
+    print(insert_interval([[1, 3], [6, 9]], [2, 5]))
