cfce660242ad99c66a9a4a2f2991edcee8433e06
Nathin78 <nathinan30115@gmail.com>
2026-07-12 13:30:17 +0530

    feat(60-days): Add Day 40 - Non-overlapping Intervals [2026-07-12]

---
+++ contributions_60_days/day40_non_overlapping_intervals.py
@@ -0,0 +1,11 @@
+﻿def erase_overlap_intervals(intervals):
+    intervals.sort(key=lambda x: x[1])
+    end = float('-inf')
+    count = 0
+    for interval in intervals:
+        if interval[0] >= end: end = interval[1]
+        else: count += 1
+    return count
+
+if __name__ == "__main__":
+    print(erase_overlap_intervals([[1, 2], [2, 3], [3, 4], [1, 3]]))
