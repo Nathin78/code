3cb78a441db79d6370d3bcd787ad095cecf9cd13
Nathin78 <nathinan30115@gmail.com>
2026-07-13 13:30:17 +0530

    feat(60-days): Add Day 41 - Rotate Image Matrix [2026-07-13]

---
+++ contributions_60_days/day41_rotate_matrix.py
@@ -0,0 +1,8 @@
+﻿def rotate_matrix(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n): matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n): matrix[i].reverse()
+
+if __name__ == "__main__":
+    m = [[1, 2], [3, 4]]; rotate_matrix(m); print(m)
