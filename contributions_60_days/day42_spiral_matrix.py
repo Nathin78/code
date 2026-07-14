efc9731b71e595254937011a85832749beb7bedb
Nathin78 <nathinan30115@gmail.com>
2026-07-14 13:30:17 +0530

    feat(60-days): Add Day 42 - Spiral Matrix Traversal [2026-07-14]

---
+++ contributions_60_days/day42_spiral_matrix.py
@@ -0,0 +1,19 @@
+﻿def spiral_order(matrix):
+    res = []
+    if not matrix: return res
+    top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix[0]) - 1
+    while top <= bottom and left <= right:
+        for c in range(left, right + 1): res.append(matrix[top][c])
+        top += 1
+        for r in range(top, bottom + 1): res.append(matrix[r][right])
+        right -= 1
+        if top <= bottom:
+            for c in range(right, left - 1, -1): res.append(matrix[bottom][c])
+            bottom -= 1
+        if left <= right:
+            for r in range(bottom, top - 1, -1): res.append(matrix[r][left])
+            left += 1
+    return res
+
+if __name__ == "__main__":
+    print(spiral_order([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
