811a8b7c710ba2da4d1b4c31822ec291064708e0
Nathin78 <nathinan30115@gmail.com>
2026-07-21 13:30:17 +0530

    feat(60-days): Add Day 49 - Sliding Window Maximum Deque [2026-07-21]

---
+++ contributions_60_days/day49_sliding_window_maximum.py
@@ -0,0 +1,14 @@
+﻿from collections import deque
+
+def max_sliding_window(nums, k):
+    q = deque()
+    res = []
+    for i, cur in enumerate(nums):
+        while q and nums[q[-1]] <= cur: q.pop()
+        q.append(i)
+        if q[0] == i - k: q.popleft()
+        if i >= k - 1: res.append(nums[q[0]])
+    return res
+
+if __name__ == "__main__":
+    print(max_sliding_window([1, 3, -1, -3, 5, 3, 6, 7], 3))
