ad78cf6ff97b38a55765b3e6843652a1efb89f8d
Nathin78 <nathinan30115@gmail.com>
2026-07-24 13:30:17 +0530

    feat(60-days): Add Day 52 - Subsets Generation Backtracking [2026-07-24]

---
+++ contributions_60_days/day52_subsets.py
@@ -0,0 +1,13 @@
+﻿def subsets(nums):
+    res = []
+    def backtrack(start, curr):
+        res.append(list(curr))
+        for i in range(start, len(nums)):
+            curr.append(nums[i])
+            backtrack(i + 1, curr)
+            curr.pop()
+    backtrack(0, [])
+    return res
+
+if __name__ == "__main__":
+    print(subsets([1, 2, 3]))
