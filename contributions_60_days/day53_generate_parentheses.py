46d108a984399b6934cc4cd60e6f8cb63ce79923
Nathin78 <nathinan30115@gmail.com>
2026-07-25 13:30:17 +0530

    feat(60-days): Add Day 53 - Generate Parentheses Backtracking [2026-07-25]

---
+++ contributions_60_days/day53_generate_parentheses.py
@@ -0,0 +1,15 @@
+﻿def generate_parenthesis(n):
+    res = []
+    def backtrack(open_c, close_c, path):
+        if len(path) == 2 * n:
+            res.append(path)
+            return
+        if open_c < n:
+            backtrack(open_c + 1, close_c, path + "(")
+        if close_c < open_c:
+            backtrack(open_c, close_c + 1, path + ")")
+    backtrack(0, 0, "")
+    return res
+
+if __name__ == "__main__":
+    print(generate_parenthesis(3))
