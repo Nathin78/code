e6623a929638d68f0d044052fb4730cdb178fc16
Nathin78 <nathinan30115@gmail.com>
2026-07-28 13:30:17 +0530

    feat(60-days): Add Day 56 - Target Sum DP Backtracking [2026-07-28]

---
+++ contributions_60_days/day56_target_sum.py
@@ -0,0 +1,12 @@
+﻿def find_target_sum_ways(nums, target):
+    dp = {0: 1}
+    for num in nums:
+        next_dp = {}
+        for s in dp:
+            next_dp[s + num] = next_dp.get(s + num, 0) + dp[s]
+            next_dp[s - num] = next_dp.get(s - num, 0) + dp[s]
+        dp = next_dp
+    return dp.get(target, 0)
+
+if __name__ == "__main__":
+    print(find_target_sum_ways([1, 1, 1, 1, 1], 3))
