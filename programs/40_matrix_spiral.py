ce20bff4c925c180c74b4183450516c0c0636936
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:32:47 +0530

    feat(programs): Add matrix spiral solution [40/50]

---
+++ programs/40_matrix_spiral.py
@@ -0,0 +1,10 @@
+﻿# Spiral order traversal of a matrix
+def spiral_order(matrix):
+    result = []
+    while matrix:
+        result += matrix.pop(0)
+        matrix = list(zip(*matrix))[::-1]
+    return result
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(spiral_order(matrix))  # [1,2,3,6,9,8,7,4,5]
