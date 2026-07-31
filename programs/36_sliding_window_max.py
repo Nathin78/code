47a305cda4555327885d74a122a3d672e31a1967
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:27:50 +0530

    feat(programs): Add sliding window max solution [36/50]

---
+++ programs/36_sliding_window_max.py
@@ -0,0 +1,17 @@
+﻿# Sliding window maximum using deque
+from collections import deque
+
+def max_sliding_window(nums, k):
+    dq = deque()
+    result = []
+    for i, num in enumerate(nums):
+        while dq and dq[0] < i - k + 1:
+            dq.popleft()
+        while dq and nums[dq[-1]] < num:
+            dq.pop()
+        dq.append(i)
+        if i >= k - 1:
+            result.append(nums[dq[0]])
+    return result
+
+print(max_sliding_window([1,3,-1,-3,5,3,6,7], 3))  # [3,3,5,5,6,7]
