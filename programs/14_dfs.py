3b86524114e78387b38ffac8f848179797a8e4a4
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:18:24 +0530

    feat(programs): Add dfs solution [14/50]

---
+++ programs/14_dfs.py
@@ -0,0 +1,13 @@
+﻿# Depth First Search on a graph
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(node)
+    result = [node]
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            result.extend(dfs(graph, neighbor, visited))
+    return result
+
+graph = {0:[1,2], 1:[0,3,4], 2:[0,5], 3:[1], 4:[1], 5:[2]}
+print(dfs(graph, 0))  # [0, 1, 3, 4, 2, 5]
