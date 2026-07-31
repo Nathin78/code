609a71c0e0a49d01138e26ca1ef1f03397792760
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:26:48 +0530

    feat(programs): Add prime sieve solution [34/50]

---
+++ programs/34_prime_sieve.py
@@ -0,0 +1,12 @@
+﻿# Sieve of Eratosthenes - find all primes up to n
+def sieve(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
+    return [i for i in range(n+1) if is_prime[i]]
+
+print(sieve(50))
+# [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
