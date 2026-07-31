34d84e3c1f3080fee4c52666b7ee935b7a113d1d
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:24:22 +0530

    feat(programs): Add missing number solution [23/50]

---
+++ programs/23_missing_number.py
@@ -0,0 +1,13 @@
+﻿# Find missing number in array [0..n]
+def missing_number(nums):
+    n = len(nums)
+    return n * (n + 1) // 2 - sum(nums)
+
+def missing_number_xor(nums):
+    result = len(nums)
+    for i, num in enumerate(nums):
+        result ^= i ^ num
+    return result
+
+print(missing_number([3,0,1]))       # 2
+print(missing_number_xor([9,6,4,2,3,5,7,0,1]))  # 8
