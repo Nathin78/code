86d7b9052a6cadfeca7f51eb28eb380d138919ac
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:13:26 +0530

    feat(programs): Add fibonacci solution [4/50]

---
+++ programs/04_fibonacci.py
@@ -0,0 +1,14 @@
+﻿# Fibonacci sequence - iterative and recursive
+def fib_iterative(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+def fib_recursive(n):
+    if n <= 1:
+        return n
+    return fib_recursive(n-1) + fib_recursive(n-2)
+
+for i in range(10):
+    print(fib_iterative(i), end=" ")  # 0 1 1 2 3 5 8 13 21 34
