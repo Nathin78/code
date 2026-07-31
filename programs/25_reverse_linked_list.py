452f27287ff8e357cce1e4de58835d188a67c330
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:24:50 +0530

    feat(programs): Add reverse linked list solution [25/50]

---
+++ programs/25_reverse_linked_list.py
@@ -0,0 +1,25 @@
+﻿# Reverse a singly linked list
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def reverse_list(head):
+    prev = None
+    curr = head
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev
+
+def to_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+head = Node(1); head.next = Node(2); head.next.next = Node(3)
+print(to_list(reverse_list(head)))  # [3, 2, 1]
