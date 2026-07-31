85290999614dc144eb0494d283474224ebbbd291
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:18:06 +0530

    feat(programs): Add bfs solution [13/50]

---
+++ programs/13_bfs.py
@@ -0,0 +1,19 @@
+﻿# Breadth First Search on a graph
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    order = []
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+    return order
+
+graph = {0:[1,2], 1:[0,3,4], 2:[0,5], 3:[1], 4:[1], 5:[2]}
+print(bfs(graph, 0))  # [0, 1, 2, 3, 4, 5]
