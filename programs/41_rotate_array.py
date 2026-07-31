50f2e0997924ab7d6519254850462e29fcf1733f
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:32:56 +0530

    feat(programs): Add rotate array solution [41/50]

---
+++ programs/41_rotate_array.py
@@ -0,0 +1,15 @@
+﻿# Rotate array to the right by k steps
+def rotate(nums, k):
+    k = k % len(nums)
+    nums[:] = nums[-k:] + nums[:-k]
+    return nums
+
+def rotate_reverse(nums, k):
+    k %= len(nums)
+    nums.reverse()
+    nums[:k] = reversed(nums[:k])
+    nums[k:] = reversed(nums[k:])
+    return nums
+
+print(rotate([1,2,3,4,5,6,7], 3))        # [5,6,7,1,2,3,4]
+print(rotate_reverse([1,2,3,4,5,6,7],3)) # [5,6,7,1,2,3,4]
