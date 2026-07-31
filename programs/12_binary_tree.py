57f72e02ad14ce41f9f638b3638358dd344f46b9
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:17:55 +0530

    feat(programs): Add binary tree solution [12/50]

---
+++ programs/12_binary_tree.py
@@ -0,0 +1,21 @@
+﻿# Binary Tree with inorder, preorder, postorder traversal
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end=" ")
+        inorder(root.right)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print("Inorder:", end=" ")
+inorder(root)  # 4 2 5 1 3
