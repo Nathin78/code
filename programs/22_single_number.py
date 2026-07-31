482c63ff7afda04ccbd8208d57ca1bb3f04c7594
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:24:05 +0530

    feat(programs): Add single number solution [22/50]

---
+++ programs/22_single_number.py
@@ -0,0 +1,9 @@
+﻿# Single Number - find element that appears once (XOR trick)
+def single_number(nums):
+    result = 0
+    for num in nums:
+        result ^= num
+    return result
+
+print(single_number([2, 2, 1]))          # 1
+print(single_number([4, 1, 2, 1, 2]))   # 4
