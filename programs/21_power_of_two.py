ec1bfd0bb432641c957a73bdf201cdbb846dea71
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:23:35 +0530

    feat(programs): Add power of two solution [21/50]

---
+++ programs/21_power_of_two.py
@@ -0,0 +1,8 @@
+﻿# Check if a number is a power of two using bit manipulation
+def is_power_of_two(n):
+    return n > 0 and (n & (n - 1)) == 0
+
+for i in range(0, 20):
+    if is_power_of_two(i):
+        print(i, end=" ")  # 1 2 4 8 16
+print()
