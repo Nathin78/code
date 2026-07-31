12599068923a1a7908ad6342df3fcb1dfced7b70
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:34:19 +0530

    feat(programs): Add word break solution [50/50]

---
+++ programs/50_word_break.py
@@ -0,0 +1,15 @@
+﻿# Word Break - check if string can be segmented using dictionary
+def word_break(s, word_dict):
+    word_set = set(word_dict)
+    dp = [False] * (len(s) + 1)
+    dp[0] = True
+    for i in range(1, len(s) + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in word_set:
+                dp[i] = True
+                break
+    return dp[len(s)]
+
+print(word_break("leetcode", ["leet","code"]))         # True
+print(word_break("applepenapple", ["apple","pen"]))    # True
+print(word_break("catsandog", ["cats","dog","sand","an","cat"]))  # False
