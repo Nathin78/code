cc99da907a4a5a302d2995060dd3b00b58e9bff8
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:24:37 +0530

    feat(programs): Add move zeroes solution [24/50]

---
+++ programs/24_move_zeroes.py
@@ -0,0 +1,11 @@
+﻿# Move all zeroes to end while maintaining order
+def move_zeroes(nums):
+    pos = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[pos], nums[i] = nums[i], nums[pos]
+            pos += 1
+    return nums
+
+print(move_zeroes([0,1,0,3,12]))  # [1, 3, 12, 0, 0]
+print(move_zeroes([0,0,1]))       # [1, 0, 0]
