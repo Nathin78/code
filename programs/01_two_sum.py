388d6d380c786f0ea6058c69fd565b52a508636d
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:12:36 +0530

    feat(programs): Add two sum solution [1/50]

---
+++ programs/01_two_sum.py
@@ -0,0 +1,12 @@
+﻿# Two Sum - Find indices of two numbers that add to target
+def two_sum(nums, target):
+    seen = {}
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in seen:
+            return [seen[diff], i]
+        seen[n] = i
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))   # [0, 1]
+print(two_sum([3, 2, 4], 6))        # [1, 2]
