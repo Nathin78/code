50e9eb1dfe0bb4442e40d20d413d92a6ad34647f
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:14:46 +0530

    feat(programs): Add linked list solution [9/50]

---
+++ programs/09_linked_list.py
@@ -0,0 +1,30 @@
+﻿# Singly Linked List implementation
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node; return
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = new_node
+
+    def display(self):
+        cur = self.head
+        while cur:
+            print(cur.data, end=" -> ")
+            cur = cur.next
+        print("None")
+
+ll = LinkedList()
+for v in [1, 2, 3, 4, 5]:
+    ll.append(v)
+ll.display()
