2c95358e4cc873a98cbacb7ec3ad5760e63ca1c4
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:25:09 +0530

    feat(programs): Add detect cycle solution [26/50]

---
+++ programs/26_detect_cycle.py
@@ -0,0 +1,18 @@
+﻿# Detect cycle in a linked list (Floyd cycle detection)
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+head = Node(1); head.next = Node(2); head.next.next = Node(3)
+head.next.next.next = head.next   # cycle
+print(has_cycle(head))  # True
