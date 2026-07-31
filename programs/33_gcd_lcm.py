2d8db323987f0624175bea1e3e6b5fa9f452c4c4
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:26:40 +0530

    feat(programs): Add gcd lcm solution [33/50]

---
+++ programs/33_gcd_lcm.py
@@ -0,0 +1,13 @@
+﻿# GCD and LCM using Euclid algorithm
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(48, 18))   # 6
+print(lcm(4, 6))     # 12
+print(gcd(100, 75))  # 25
+print(lcm(12, 18))   # 36
