45000de2d9d7919cbd65abde696f73a4b302b747
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:13:37 +0530

    feat(programs): Add factorial solution [5/50]

---
+++ programs/05_factorial.py
@@ -0,0 +1,14 @@
+﻿# Factorial - iterative and recursive
+def factorial_iterative(n):
+    result = 1
+    for i in range(2, n+1):
+        result *= i
+    return result
+
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial_recursive(n-1)
+
+print(factorial_iterative(5))   # 120
+print(factorial_recursive(6))   # 720
