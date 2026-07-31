904e3efe297129885094961a364542d3ffa44dc8
Nathin78 <nathinan30115@gmail.com>
2026-07-31 19:32:29 +0530

    feat(programs): Add prefix sum solution [38/50]

---
+++ programs/38_prefix_sum.py
@@ -0,0 +1,14 @@
+﻿# Prefix Sum for range sum queries
+class PrefixSum:
+    def __init__(self, nums):
+        self.prefix = [0] * (len(nums) + 1)
+        for i, n in enumerate(nums):
+            self.prefix[i+1] = self.prefix[i] + n
+
+    def range_sum(self, left, right):
+        return self.prefix[right+1] - self.prefix[left]
+
+ps = PrefixSum([1, 2, 3, 4, 5])
+print(ps.range_sum(0, 2))  # 6  (1+2+3)
+print(ps.range_sum(1, 3))  # 9  (2+3+4)
+print(ps.range_sum(0, 4))  # 15 (sum all)
