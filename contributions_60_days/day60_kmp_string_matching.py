5f4428a1b45eb8a9e3bd310166f0844734d6c2bd
Nathin78 <nathinan30115@gmail.com>
2026-08-01 13:30:17 +0530

    feat(60-days): Add Day 60 - KMP String Pattern Matching [2026-08-01]

---
+++ contributions_60_days/day60_kmp_string_matching.py
@@ -0,0 +1,33 @@
+﻿def kmp_search(pattern, text):
+    def compute_lps(p):
+        lps = [0] * len(p)
+        length = 0
+        i = 1
+        while i < len(p):
+            if p[i] == p[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            elif length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+        return lps
+
+    lps = compute_lps(pattern)
+    i = j = 0
+    res = []
+    while i < len(text):
+        if pattern[j] == text[i]:
+            i += 1; j += 1
+        if j == len(pattern):
+            res.append(i - j)
+            j = lps[j - 1]
+        elif i < len(text) and pattern[j] != text[i]:
+            if j != 0: j = lps[j - 1]
+            else: i += 1
+    return res
+
+if __name__ == "__main__":
+    print(kmp_search("ABABCABAB", "ABABDABACDABABCABAB"))
